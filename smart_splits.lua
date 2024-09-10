6cc64fc6791354fbbbdfdd5c1e981be0cf4edea1
Nicolas Stirnemann <nicolas@brainhi.com>
2024-09-10 19:46:59 -0300

    Added keys to splits, untracked workspace_config

--- smart_splits.lua
+++ smart_splits.lua
@@ -3,7 +3,13 @@ local M = {}
 function M.smart_split(wezterm, config)
 	local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 
-	smart_splits.apply_to_config(config)
+	smart_splits.apply_to_config(config, {
+		direction_keys = { "h", "j", "k", "l" },
+		modifiers = {
+			move = "CTRL",
+			resize = "META",
+		},
+	})
 end
 
 return M
